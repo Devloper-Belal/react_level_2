d6b2f89716e2ef8c765e0bb6e3d33f3acbb94d5a
github_Devloper-Belal <asaamman123.bk@gmail.com>
2023-01-22 06:04:50 +0200

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setname] = useState("ali hassan");
+  const [age, setage] = useState(18);
+  const [count, setcount] = useState(0);
+  
+  const ages = () => {
+    setage(33);
+    
+  }
+
+    
+  const countsf = () => {
+    setcount(count + 1);
+    
+  }
+
   return (
     <div className="App">
-      <button>count is 0</button>
+      <h1>my name is {name}</h1>
+      <button onClick={() => setname("belal balasmeh♣")}>Change Name</button>
+      <br />
+      <h1>my age is {age}</h1>
+      <button onClick={ages}>Change age</button>
+      <br />    <br />
+      <h1>my count is {count}</h1>
+      <button onClick={countsf}>Change age</button>
     </div>
   );
 }
