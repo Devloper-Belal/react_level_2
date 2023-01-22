fc9749881749bc058090130bc28589fe9913acbe
github_Devloper-Belal <asaamman123.bk@gmail.com>
2023-01-22 10:55:56 +0200

    darck & light mpde

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import "./theme.css";
 import { useState } from "react";
+
 
 function App() {
   const [name, setname] = useState("ali hassan");
   const [age, setage] = useState(18);
   const [count, setcount] = useState(0);
+  const [theme, settheme] = useState("");
   
   const ages = () => {
     setage(33);
@@ -16,17 +19,29 @@ function App() {
     setcount(count + 1);
     
   }
-
+  
   return (
-    <div className="App">
-      <h1>my name is {name}</h1>
+  
+    <div className={`App ${theme}` }>
+            
+  <div className="flexxx">
+    
+           <button onClick={()=>settheme("light")} style={{marginRight:"20px"}}>lagit</button>
+            <button onClick={()=>settheme("darck")} style={{marginRight:"20px"}}>darck</button>
+            <button onClick={()=>settheme("grey")} style={{marginRight:"20px"}}>grey</button>
+            <button onClick={()=>settheme("pink")} >pink</button>
+  </div>
+        
+    
+
+      <h4 style={{marginTop:"50px"}}>my name is {name}</h4>
       <button onClick={() => setname("belal balasmeh♣")}>Change Name</button>
       <br />
-      <h1>my age is {age}</h1>
+      <h4>my age is {age}</h4>
       <button onClick={ages}>Change age</button>
       <br />    <br />
-      <h1>my count is {count}</h1>
-      <button onClick={countsf}>Change age</button>
+    
+      <button onClick={countsf}>Count {count}</button>
     </div>
   );
 }
